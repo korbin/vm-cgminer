6f65242ecd9221af99687e22e99a9e1955ffaf91
voxq <t@voxq.com>
2023-04-17 14:38:41 +0200

    Fixed: accidental removal of timer. Build: changed to support Blake3 under Windows and Msys 1.0 environment

--- cgminer.c
+++ cgminer.c
@@ -6329,6 +6329,8 @@ static void hash_sole_work(struct thr_info *mythr)
 			if (hashes > cgpu->max_hashes)
 				cgpu->max_hashes = hashes;
 
+			timersub(tv_end, &tv_start, &diff);
+
 			sdiff.tv_sec += diff.tv_sec;
 			sdiff.tv_usec += diff.tv_usec;
 			if (sdiff.tv_usec > 1000000) {
